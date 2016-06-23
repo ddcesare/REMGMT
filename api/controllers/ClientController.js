a1f7bbd3a756efa34ad7c0e1014d93fd5ee725c5
dd di cesare <didi@posteo.net>
2016-06-23 20:24:47 +0200

    [api] client api, model definition

---
+++ api/controllers/ClientController.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Controller = require('trails-controller')
+
+/**
+ * @module ClientController
+ * @description Generated Trails.js Controller.
+ */
+module.exports = class ClientController extends Controller{
+
+}
+
--- api/models/index.js
+++ api/models/index.js
@@ -1,2 +1,3 @@
 'use strict'
 exports.User = require('./User')
+exports.Client = require('./Client')
