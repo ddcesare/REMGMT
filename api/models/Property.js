6f2483a2eff6962d03391f4e10036f6e863d598b
dd di cesare <didi@posteo.net>
2016-07-02 15:06:53 +0200

    [api] Property foundation

---
+++ api/models/Property.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Model = require('trails-model')
+
+/**
+ * @module Property
+ * @description Property Model
+ */
+module.exports = class Property extends Model {
+
+  static config () {
+  }
+
+  static schema () {
+
+  }
+}
